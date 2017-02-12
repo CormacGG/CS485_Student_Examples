a4c00955bff248ee9623bc7f142a2781cdcec850
maro2520 <maro2520@HOMER>
2017-02-12 15:02:53 -0800

    Done!

--- CS485_Student_Examples/01Lab_DynamicMemory/PacString.cpp
+++ CS485_Student_Examples/01Lab_DynamicMemory/PacString.cpp
@@ -5,37 +5,49 @@
 using namespace std;
 using std::swap;
 
+void copyString(char *dest, const char *src);
+void catString(char *dest, const char *src);
+
 PacString::PacString ()
 {
 
 }
 PacString::PacString (const char *pszString)
 {
-	mpszData = new char[strlen(pszString)];
-	strcpy (mpszData, pszString);
+	mpszData = new char[strlen(pszString) + 1];
+	copyString (mpszData, pszString);
 }
 PacString::PacString (const PacString &rcData)
 {
-	mpszData = new char[strlen(rcData.mpszData)];
+	mpszData = new char[strlen(rcData.mpszData) + 1];
 	
-	strcpy (mpszData, rcData.mpszData);
+	copyString (mpszData, rcData.mpszData);
 }
 PacString::~PacString ()
 {
 	delete mpszData;
 }
-/*
+
 PacString& PacString::operator=(const PacString &rcData)
 {
-	strcpy (mpszData, rcData.mpszData);
+	if (mpszData)
+	{
+		delete mpszData;
+	}
+
+	mpszData = new char[strlen(rcData.mpszData) + 1];
+
+	copyString (mpszData, rcData.mpszData);
+
+	return *this;
 }
-*/
-PacString& PacString::operator=(PacString rcData)
+
+/*PacString& PacString::operator=(PacString rcData)
 {
 	swap (mpszData, rcData.mpszData);
 
 	return *this;
-}
+}*/
 std::ostream& operator<<(std::ostream &out, const PacString &rcData)
 {
 	out << rcData.mpszData;
@@ -44,22 +56,55 @@ std::ostream& operator<<(std::ostream &out, const PacString &rcData)
 }
 PacString& PacString::operator+=(const PacString &rcData)
 {
-	char * temp = rcData.mpszData;
-	cout << "mpszData: " << strlen(mpszData) << " and rcData: " << strlen(rcData.mpszData);
-	swap(mpszData, temp);
+	char *temp = new char[strlen(mpszData) + strlen(rcData.mpszData) + 1];
+
+	copyString(temp, mpszData);
+	catString (temp, rcData.mpszData);
+
+	//delete mpszData;
+
+	//mpszData = new char[strlen(mpszData) + strlen(rcData.mpszData)];
+
+	swap(temp, mpszData);
+
+	delete temp;
 
 	return *this;
 }
 PacString PacString::operator+(const PacString &rcData) const
 {
-	PacString newString;
+	PacString *pNewString = new PacString ();
 	char * temp = mpszData;
 
-	newString.mpszData = new char[strlen(mpszData)];
+	pNewString->mpszData = new char[strlen(mpszData) + strlen(rcData.mpszData) + 1];
 
-	strcpy (newString.mpszData, mpszData);
-	strcat (newString.mpszData, rcData.mpszData);
+	copyString (pNewString->mpszData, mpszData);
+	catString (pNewString->mpszData, rcData.mpszData);
 	
 
-	return newString;
+	return *pNewString;
+}
+
+void copyString (char *dest, const char *src)
+{
+	char ch;
+
+	ch = src[0];
+
+	for (int i = 0; NULL != ch; i++)
+	{
+		ch = src[i];
+		dest[i] = ch;
+	}
+}
+
+void catString(char *dest, const char *src)
+{
+	char ch = 'a';
+
+	for (int i = strlen(dest), j = 0; NULL != ch; i++, j++)
+	{
+		ch = src[j];
+		dest[i] = ch;
+	}
 }
